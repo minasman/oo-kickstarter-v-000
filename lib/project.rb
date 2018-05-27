dd873cf91f151ba48c21b04c88a92cd3cded1fd5
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-27 21:17:45 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,14 @@
 class Project
   attr_reader :title
+  attr_accessor :backers
   
   def initialize(title)
     @title = title
+    @backers = []
   end
+  
+  def add_backer(backer)
+    self.backers << backers
+  end
+  
 end
